4537f1ea3ff0d649bac8dfe104bd72dc44207c53
mayukhdeep12 <mayukhdeep12mathur@yahoo.com>
2023-09-25 23:58:34 +0530

    IC3

--- src/App.js
+++ src/App.js
@@ -61,6 +61,9 @@ const App = () => {
         const stream = await navigator.mediaDevices.getUserMedia(constraints);
         mediaStream = stream;
         videoRef.current.srcObject = stream;
+        videoRef.setAttribute('autoplay', '');
+        videoRef.setAttribute('muted', '');
+        videoRef.setAttribute('playsinline', '');
       } catch (err) {
         console.log(err);
       }
@@ -150,10 +153,7 @@ const App = () => {
         {/* Display the video feed from the camera */}
         <video
           ref={videoRef}
-          autoPlay
           style={{ display: !image ? "block" : "none" }}
-          playsinline
-          muted
         ></video>
         <img
           ref={imageRef}
